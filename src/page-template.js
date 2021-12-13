56684b434202cbd8529a777096f2b7fe913dd243
pcancio <pcancio@gmail.com>
2021-12-12 23:41:24 -0500

    fixed more bugs

--- src/page-template.js
+++ src/page-template.js
@@ -3,9 +3,9 @@ const generateTeam = team => {
     // create manager card
     const generateManager = manager => {
         return `<div class = "card employee-card" >
-            <div class = "card-header">
-            <h2 class = "card-title"> ${manager.getName()} </h2>  
-            <h3 class = "card-title"> <i class = "fas fa-mug-hot mr-2"> </i>${manager.getRole()}</h3 >
+            <div class = "card-header bg-primary text-light text-center">
+            <h2 class = "card-title">${manager.getName()}</h2>  
+            <h3 class = "card-subtitle"><i class="fas fa-people-arrows"> ${manager.getRole()}</i></h3>      
             </div>  
             <div class = "card-body">
             <ul class = "list-group">
@@ -18,16 +18,16 @@ const generateTeam = team => {
     };
     // create engineer card
     const generateEngineer = engineer => {
-        return `<div class = "card employee-card" >
-        <div class = "card-header">
-        <h2 class = "card-title"> ${engineer.getName()} </h2>  
-        <h3 class = "card-title"> <i class = "fas fa-glasses mr-2"> </i>${engineer.getRole()}</h3>
+        return `<div class = "card employee-card">
+        <div class = "card-header bg-primary text-light text-center">
+        <h2 class = "card-title">${engineer.getName()}</h2>  
+        <h3 class = "card-subtitle"><i class="fas fa-user-astronaut"> ${engineer.getRole()}</i></h3>        
         </div>  
         <div class = "card-body">
         <ul class = "list-group">
         <li class = "list-group-item"> ID: ${ engineer.getId()} </li>  
         <li class = "list-group-item"> Email: <a href = "mailto:${engineer.getEmail()}"> ${engineer.getEmail()} </a> </li>
-        <li class = "list-group-item"> Github username: ${engineer.getGithub()}</li>
+        <li class = "list-group-item"> Github: <a href = "https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a> </li>
         </ul> 
         </div> 
         </div>`;
@@ -35,12 +35,11 @@ const generateTeam = team => {
     // create intern card
     const generateIntern = intern => {
         return `<div class = "card employee-card">
-    <div class = "card-header">
-    <h2 class = "card-title"> ${intern.getName()} </h2>  
-    <h3 class = "card-title"> <i class = "fas fa-user-graduate mr-2"> </i>${intern.getRole()}</h3 >
+    <div class = "card-header bg-primary text-light text-center">
+    <h2 class = "card-title">${intern.getName()}</h2>  
+    <h3 class = "card-subtitle"> <i class="fas fa-rocket"> ${intern.getRole()}</i> </h3 >   
     </div>  
     <div class = "card-body" >
-
     <ul class = "list-group" >
     <li class = "list-group-item" > ID: ${ intern.getId()} </li>  
     <li class = "list-group-item" > Email: <a href = "mailto:${intern.getEmail()}"> ${intern.getEmail()} </a> </li>
@@ -75,18 +74,20 @@ module.exports = teamMembers => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Team Profile Generator</title>
-        <link rel="stylesheet" href="https:://stackpath.bottstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    </head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/cc59cd1bed.js" crossorigin="anonymous"></script>
+        </head>
     <body>
-        <header class ="container-fluid">
-        <div class ="row">
-        <div class ="col-12 jumbotron mb-3 team-heading">
-        <h1 class ="text-center">Team Profile</h1>
+    <header class ="container-fluid">
+    <div class= "row">
+    <div class="col-12 jumbotron mb-3 team-heading">
+        <h1 class ="text-center">My Team</h1>
         </div>
         </div>
-        </div>
+        </header>
+      
         <div class = "container">
-        <div class = "row-flex">
+        <div class = "row">
         <div class = "team-area col-12 d-flex justify-content-center">
         ${generateTeam(teamMembers)}
         </div>
